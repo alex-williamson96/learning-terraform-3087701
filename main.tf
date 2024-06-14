714ec5a723dad6c61bd8f39bee91fefec519efda
Alex Williamson <85185426+alex-williamson96@users.noreply.github.com>
2024-06-14 15:08:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,10 +46,10 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.if
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
-  from_port   = 80
-  to_port     = 80
+  from_port   = 443
+  to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
