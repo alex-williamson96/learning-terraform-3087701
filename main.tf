628344619a9ae78e5d4a54cb14d88a68debde7fa
Alex Williamson <85185426+alex-williamson96@users.noreply.github.com>
2024-06-19 14:18:01 -0400

    remove listeners

--- main.tf
+++ main.tf
@@ -56,15 +56,13 @@ module "blog-alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    http_tcp_listeners = [
-      {
-        port                = 80
-        protocol            = "HTTP"
-        target_group_index  = 0
-      }
-    ]
-  }
+  http_tcp_listeners = [
+    {
+      port                = 80
+      protocol            = "HTTP"
+      target_group_index  = 0
+    }
+  ]
 
   target_groups = [
     {
