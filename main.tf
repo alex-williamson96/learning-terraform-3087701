a3bb17617009b38459c449deb6f9ddc202dbd0e3
Alex Williamson <85185426+alex-williamson96@users.noreply.github.com>
2024-06-19 15:58:08 -0400

    update listners

--- main.tf
+++ main.tf
@@ -56,11 +56,15 @@ module "alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    http_tcp_listeners = {
-      port                = 80
-      protocol            = "HTTP"
-      target_group_index  = 0
+listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
     }
   }
 
