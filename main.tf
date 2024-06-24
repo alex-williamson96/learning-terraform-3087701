29c295077f38f05b106ccbf50418da59ebde89a6
Alex Williamson <85185426+alex-williamson96@users.noreply.github.com>
2024-06-24 14:09:50 -0400

    change autoscaling version

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "blog_vpc" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "7.6.1"
+  version = "6.5.2"
   # insert the 1 required variable here
 
   name = "blog"
